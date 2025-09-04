f1b829053b5c86c67845f752c31718ae0caaf3f9
gabby <hannahfrench1709@hotmail.com>
2025-09-04 16:24:31 +0100

    feat: set up server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 //imports
+import express from "express";
+import cors from "cors";
+import { db } from "./database-connection.js";
 
-//initialise express and cors
+//start express
+const app = express();
 
-//tell server to use JSON
+//config express with JSON and cors
+app.use(express.json());
+app.use(cors());
+
+//set up a port
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
 
 //set up a root route 
+app.get("/", (request, response) => {
+    response.json({ message: "Welcome to the server. GET comfy!"});
+}); 
 
 //===========================================
 
