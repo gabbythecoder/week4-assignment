bd62496be2be4c62895ee3e2cf570b4b1527fbf2
gabby <hannahfrench1709@hotmail.com>
2025-09-04 15:39:40 +0100

    feat: add imports and set up pool

--- server/database-connection.js
+++ server/database-connection.js
@@ -1,7 +1,14 @@
-//imports 
+//import pg and dotenv
+import pg from "pg";
+import dotenv from "dotenv";
 
 //configure dotenv
+dotenv.config();
 
 //get your connection string value from .env file
+const dbConnectionString = process.env.DB_URL;
 
 //set up a pool
+export const db = new pg.Pool({
+    connectionString: dbConnectionString,
+});
