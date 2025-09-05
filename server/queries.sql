8cb7436c7dbaaefb33b61bd8a9f1bd1ea831fc56
gabby <hannahfrench1709@hotmail.com>
2025-09-05 21:26:39 +0100

    feat: add dummy data in queries.sql

---
+++ server/queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE movie_reviews (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    movie_title TEXT NOT NULL,
+    user_name TEXT,
+    review TEXT,
+    watch_again BOOLEAN DEFAULT FALSE,
+    rating INT CHECK (rating BETWEEN 1 and 5),
+    created_at TIMESTAMPTZ DEFAULT NOW()
+)
+
+--testing: adding new data to table
+INSERT INTO movie_reviews (movie_title, user_name, review, watch_again, rating)
+VALUES ('The Conjuring', 'Bob', 'Too many jump scares for me', FALSE, 3), ('Scream', 'Dana', 'Best slasher movie ever! Will definitely rewatch', TRUE, 5), ('Halloween', 'Ross', 'Movie was not scary at all!', FALSE, 2)
+
+
+SELECT * FROM movie_reviews;
