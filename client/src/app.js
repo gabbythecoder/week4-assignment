f7437cd34ca8e21fc5b98414c79ed3bb15c61a2a
gabby <hannahfrench1709@hotmail.com>
2025-09-07 21:42:27 +0100

    feat: update comments

--- client/src/app.js
+++ client/src/app.js
@@ -5,19 +5,13 @@ const movieReviewForm = document.getElementById("movie-review-form");
 // - add an event to this HTML element
 movieReviewForm.addEventListener("submit", handleSubmit);
 
-// - the event handler has the following steps:
-//      - prevent the default behaviour of the event
-//      - create template for user's data
-//      - fill in the template with user's data
-//      - send data (JSON) to the server -> while in development, the server url uses local host, but when you finish deploying and your project is done, you need to replace the local host url with the server deployed url (from render)
-
 function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(movieReviewForm);
     const formValues = Object.fromEntries(formData);
     console.log(formValues);
 
-    //need to update this to the correct URL - this is just for testing
+    //updated with correct URL 
     fetch("https://week4-assignment-1bct.onrender.com/add-movie-reviews", {
         method: "POST",
         headers: {
