c86fefc8e8ca22c9b41c85921575be1a6cb00a45
gabby <hannahfrench1709@hotmail.com>
2025-09-07 14:43:34 +0100

    feat: update queries.sql with new column added

--- server/queries.sql
+++ server/queries.sql
@@ -8,6 +8,10 @@ CREATE TABLE movie_reviews (
     created_at TIMESTAMPTZ DEFAULT NOW()
 )
 
+--adding new column for likes in movie_reviews table
+ALTER TABLE movie_reviews 
+    ADD COLUMN likes INTEGER DEFAULT 0;
+
 --testing: adding new data to table
 INSERT INTO movie_reviews (movie_title, user_name, review, watch_again, rating)
 VALUES ('The Conjuring', 'Bob', 'Too many jump scares for me', FALSE, 3), ('Scream', 'Dana', 'Best slasher movie ever! Will definitely rewatch', TRUE, 5), ('Halloween', 'Ross', 'Movie was not scary at all!', FALSE, 2)
